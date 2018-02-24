fc01e2af730345c266b1b1d7c4035202129adfb5
Anton Korzunov <thekashey@gmail.com>
2018-02-25 08:41:27 +1100

    npm seo

--- src/create.js
+++ src/create.js
@@ -1,6 +1,5 @@
 import scan from 'scan-directory';
 
-
 export default async function look(root) {
   const __mocks__ = await scan(root, (file) => file.match(/__mocks__/));
   return __mocks__.map(file => ({
